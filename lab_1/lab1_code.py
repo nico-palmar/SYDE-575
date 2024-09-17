dd5a6dccf85ceee847d5fc1815ffb6832f171f74
sophiehermanns <75340449+sophiehermanns@users.noreply.github.com>
2024-09-17 14:47:59 -0400

    final lab 1 code (hopefully) (#7)

--- lab_1/lab1_code.py
+++ lab_1/lab1_code.py
@@ -4,11 +4,6 @@ import skimage.transform as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage.exposure
-plt.gray()
-
-lena= rgb2gray(imread('lena.tiff')) *255
-cameraman = imread('cameraman.tif').astype(np.float64)
-tire = imread('tire.tif').astype(np.float64) / 255.0
 
 method_order_to_name = {
     0: "Nearest Neighbor",
@@ -58,7 +53,7 @@ def digital_zoom(image, factor, order, ax, title):
 
 def zoom_image_different_methods(image, factor):
     images = []
-    fig, axs = plt.subplots(1, 3, figsize=(15, 15))
+    fig, axs = plt.subplots(1, 3, figsize=(15, 6))
     for i in range(3):
         order = i
         if i == 2:
@@ -67,15 +62,33 @@ def zoom_image_different_methods(image, factor):
         images.append(zoomed_image)
     return images
 
+def tire_plotting(tire_image, image_title, histogram_title):
+    """
+    Plotting image and histogram of tire
+    """
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    tire_flattened = tire_image.flatten()
+    axs[0].imshow(tire_image)
+    axs[1].hist(tire_flattened)
+    axs[0].set_title(image_title)
+    axs[1].set_title(histogram_title)
+    axs[1].set_xlabel("Pixel Intensity (k)")
+    axs[1].set_ylabel("Pixel Count (n_k)")
+    plt.show()
+
+lena= rgb2gray(imread('lena.tiff')) *255
+cameraman = imread('cameraman.tif').astype(np.float64)
+tire = imread('tire.tif').astype(np.float64) / 255.0
 
 # plot original images
-fig, axs = plt.subplots(1, 2, figsize=(8, 8))
+fig, axs = plt.subplots(1, 2, figsize=(8, 4))
 for i, image in enumerate([lena, cameraman]):
     axs[i].imshow(image, cmap='gray')
 
 fig.suptitle('Lena and Cameraman Original Images')
-plt.subplots_adjust(top=1.4)
+# plt.subplots_adjust(top=1.4)
 plt.show()
+plt.gray()
 
 # reduce resolution and plot
 lena_reduced = reduce_resolution(lena, "Lena Reduced Resolution", 4)
@@ -102,3 +115,23 @@ for i, c_image in enumerate(cameraman_images):
         order = 3
     upsample_method = method_order_to_name[order]
     print(f"PSNR between Cameraman image and up-sampled image using {upsample_method.lower()} interpolation: {PSNR(cameraman, c_image)}")
+
+# plot original tire image and histogram
+tire_plotting(tire, "Tire Image Scaled Down to Range [0.0,1.0]", "Tire Histogram Scaled Down to Range [0.0,1.0]")
+
+# calculate negative
+tire_negative = 1.0 - tire
+tire_plotting(tire_negative, "Negative Tire Image", "Negative Tire Histogram")
+
+# calculate power-law transformation for gamma 0.5
+I_out1 = tire ** 0.5
+tire_plotting(I_out1, "Gamma Corrected (γ=0.5) Tire Image", "Gamma Corrected (γ=0.5) Tire Histogram")
+
+# calculate power-law transformation for gamma 1.3
+I_out2 = tire ** 1.3
+tire_plotting(I_out2, "Gamma Corrected (γ=1.3) Tire Image", "Gamma Corrected (γ=1.3) Tire Histogram")
+
+# apply histogram equalization
+hist_equalizer = skimage.exposure.equalize_hist(tire)
+tire_plotting(hist_equalizer, "Tire Image Histogram Equalization", "Tire Histogram Equalization")
+
